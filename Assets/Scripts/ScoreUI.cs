0bc5e70de7a6934faba370c6317e2644c22857ce
Shamith S Kumar <shamithshasikumar@gmail.com>
2026-06-09 12:54:13 +0530

    Level added in UI

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -6,8 +6,12 @@ public class ScoreUI : MonoBehaviour
     [SerializeField]
     private TMP_Text scoreText;
 
+    [SerializeField]
+    private TMP_Text levelText;
+
     private void Update()
     {
         scoreText.text = "Score : " + ScoreManager.Instance.Score;
+        levelText.text = "Level : " + LevelManager.Instance.CurrentLevel;
     }
 }
